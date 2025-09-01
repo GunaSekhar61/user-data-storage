9b44a216df56779cf0a28f470d6377047ea08119
GunaSekhar61 <61678256+GunaSekhar61@users.noreply.github.com>
2025-09-01 10:32:18 +0530

    Add files via upload

---
+++ user-info-storage/src/test/java/com/user_data_storage/user_info_storage/UserInfoStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_data_storage.user_info_storage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserInfoStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
